e77a56b057558773133648cbb9556568fd95edb0
Junaid <qjunaid623@gmail.com>
2020-11-14 23:19:22 +0500

    All Styles done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,17 @@ const Footer = () => {
     return(
         <div className='footer-container' >
             <h1>Developed by : Junaid Qureshi</h1>
+            <div className="icons-container">
+                <a title='portfolio' href="https://developer-junaid.web.app/" rel="noopener noreferrer" target="_blank">
+                    <i className="fas fa-address-card portfolio"></i>
+                </a>
+                <a title='github' href="https://github.com/developer-junaid/" rel="noopener noreferrer" target="_blank">
+                    <i className="fab fa-github-square github"></i>
+                </a>
+                <a title='linkedin' href="https://www.linkedin.com/in/developer-junaid/" rel="noopener noreferrer" target="_blank">
+                    <i className="fab fa-linkedin linkedin"></i>
+                </a>
+             </div>
         </div>
     )
 }
