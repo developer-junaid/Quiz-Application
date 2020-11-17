d4be2a034db485bf25c01d1cac65115577bf5294
Junaid <qjunaid623@gmail.com>
2020-11-17 22:36:44 +0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,15 @@
 // the cache version gets updated every time there is a new deployment
-const CURRENT_CACHE = "quiz"
-const self = this
+const CACHE_VERSION = 10
+const CURRENT_CACHE = `main-${CACHE_VERSION}`
 
 // these are the routes we are going to cache for offline support
-const cacheFiles = ["/", "/offline/"]
+const cacheFiles = ["/", "/offline/", "manifest.json"]
 
 // on activation we clean up the previously registered service workers
-self.addEventListener("activate", (evt) =>
+this.addEventListener("activate", (evt) =>
   evt.waitUntil(
     caches.keys().then((cacheNames) => {
-      return Promise.all(
+      return Promise.all(zz
         cacheNames.map((cacheName) => {
           if (cacheName !== CURRENT_CACHE) {
             return caches.delete(cacheName)
@@ -21,7 +21,7 @@ self.addEventListener("activate", (evt) =>
 )
 
 // on install we download the routes we want to cache for offline
-self.addEventListener("install", (evt) =>
+this.addEventListener("install", (evt) =>
   evt.waitUntil(
     caches.open(CURRENT_CACHE).then((cache) => {
       return cache.addAll(cacheFiles)
@@ -60,7 +60,7 @@ const update = (request) =>
 
 // general strategy when making a request (eg if online try to fetch it
 // from the network with a timeout, if something fails serve from cache)
-self.addEventListener("fetch", (evt) => {
+this.addEventListener("fetch", (evt) => {
   evt.respondWith(
     fromNetwork(evt.request, 10000).catch(() => fromCache(evt.request))
   )
