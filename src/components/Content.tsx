674543e7d847d6669da9c689716e266a5c4f4bbc
Junaid <qjunaid623@gmail.com>
2020-11-19 15:56:38 +0500

    Done !!

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -24,7 +24,6 @@ const Content = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      // const dummyResponse = await fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple")
       if (sendRequest) {
         console.log("getting data ...")
         setGettingData(true)
--- src/swDev.js
+++ src/swDev.js
@@ -1,13 +1,37 @@
 export const register = () => {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
-      navigator.serviceWorker.register("/sw3.js").then(
+      navigator.serviceWorker.register("/sw.js").then(
         function (registration) {
           // Registration was successful
           console.log(
             "ServiceWorker registration successful with scope: ",
             registration.scope
           )
+
+          // Caching all API Data
+          let difficulties = ["easy", "medium", "hard"]
+          let amounts = [5, 10, 15]
+          let categories = [9, 11, 17, 18, 19, 21]
+
+          // loop
+          categories.map((eachCategory) => {
+            difficulties.map((eachDifficulty) => {
+              amounts.map(async (eachAmount) => {
+                await fetch(
+                  `https://opentdb.com/api.php?amount=${eachAmount}&category=${eachCategory}&difficulty=${eachDifficulty}&type=multiple`
+                )
+                  .then((success) => {
+                    console.log("success")
+                  })
+                  .catch((err) => {
+                    console.log("error")
+                  })
+              })
+            })
+          })
+
+          //
         },
         function (err) {
           // registration failed :(
