b57bfd530674afc590b0c2f3625f2fb848148b8e
Junaid <qjunaid623@gmail.com>
2020-11-19 19:20:54 +0500

    Changed the name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './../styles/css/header.css';
+import React from "react"
+import "./../styles/css/header.css"
 
 const Header = () => {
-    return(
-        <div className='header-container' >
-            <h1>Online Quiz Application</h1>
-        </div>
-    )
+  return (
+    <div className="header-container">
+      <h1>Our Quiz Application</h1>
+    </div>
+  )
 }
 
 export default Header
